d5d90d6aac023b7a1b8202a6c144f5243dee92eb
shipatulhasan <shipatulhasan328@gmail.com>
2022-08-07 22:27:07 +0600

    adding array practice file

---
+++ arrayPractice.js
@@ -0,0 +1,13 @@
+// write a program to get summation of an array. 
+// Has paremeter + Has return
+
+function getSum(arr){
+    let sum = 0
+    for(let i = 0; i<arr.length; i++){
+       console.log(sum,'+',arr[i],'=',(sum=sum+arr[i]))
+    }
+    return sum
+}
+const arr1 = [12, 30, 20, 11, 55]
+const mySum = getSum(arr1)
+console.log('Here is the summetion of given input: ', mySum)
